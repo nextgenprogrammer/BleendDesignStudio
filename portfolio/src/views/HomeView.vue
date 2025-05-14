1bb9f3baa6700fb55fd7dc355520a72de22596a2
Saurabh Kulkarni <tobeaprogrammer@users.noreply.github.com>
2025-05-15 02:21:12 +0530

    removing old project

--- portfolio/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <SketchSlider></SketchSlider>
-  <QuoteSection></QuoteSection>
-</template>
-
-<script setup>
-import QuoteSection from '@/components/QuoteSection.vue';
-import SketchSlider from '@/components/SketchSlider.vue';
-</script>
