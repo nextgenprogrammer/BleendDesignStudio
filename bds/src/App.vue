95c5cd9e63afb4231f9aaaa8c7d94ec20397dfc7
Saurabh Kulkarni <tobeaprogrammer@users.noreply.github.com>
2025-05-15 02:06:43 +0530

    project setup complete

---
+++ bds/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import FooterBar from './components/FooterBar.vue';
+import NavigationBar from './components/NavigationBar.vue';
+
+</script>
+
+<template class="font-sans">
+  <NavigationBar></NavigationBar>
+    <div class="main-container content-center">  
+      <router-view/>
+    </div>  
+  <FooterBar></FooterBar>
+</template>
+
+<style>
+</style>
---
+++ bds/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <SketchSlider></SketchSlider>
+  <QuoteSection></QuoteSection>
+</template>
+
+<script setup>
+import QuoteSection from '@/components/QuoteSection.vue';
+import SketchSlider from '@/components/SketchSlider.vue';
+</script>
